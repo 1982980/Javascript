88405340b370e02c88108c115b61af2f0636e269
byuzyfyshyn <Nerosman2601@gmail.com>
2021-03-18 08:58:38 +0200

    Update task2.js

--- Lesson3/task2.js
+++ Lesson3/task2.js
@@ -7,9 +7,30 @@
 //   return clone;
 // };
 
+// your work variant
+const array_Clone = function (input) {
+  const clone = [];
+  for (let value of input) {
+    clone.push(value);
+  }
+  return clone;
+};
+
+// w3resourcee
 const array_Clone = (input) => input.slice(0);
 
 console.log(array_Clone([1, 2, 4, 0]));
 console.log(array_Clone([1, 2, [4, 0]]));
 // [1, 2, 4, 0]
 // [1, 2, [4, 0]]
+
+const array_Clone = function (input) {
+  if(Array.isArray(input)) {
+    return [].concat(input)
+  }
+  return [];
+};
+
+const array_Clone = function (input) {
+  return [...input]
+};
